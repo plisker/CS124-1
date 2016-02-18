5c98ade23841afc12981a8663aa3f7b5ae783582
Paul Lisker <Paul@Pauls-Laptop.local>
2016-02-17 22:37:52 -0500

    Creating workspace

---
+++ Assignment1/Assignment1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment1
+//
+//  Created by Paul Lisker on 2/17/16.
+//  Copyright © 2016 Lisker & Saraf. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
